8bc5281365fc4c2ebb1bece05f1568fcb141830a
Matt Adams <mattadams@factor1studios.com>
2016-09-30 12:44:40 -0700

    loop error on single.

--- single.php
+++ single.php
@@ -16,11 +16,9 @@ get_header();
     <section class="flexible-content">
     <div class="row">
         <div class="small-centered small-24 medium-20 large-18 columns">
-	        
-		<?php if (have_posts()):
-			while (have_posts()): 
-			the_content();
-			endwhile; endif; ?>
+	     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>   
+		 	<?php the_content();?>
+		 	<?php endwhile; endif; ?>
         </div>
     </div>
     </section>
