978a1587acf2a8a780b48087d2eef1b643f013e5
Matt Adams <mattadams@factor1studios.com>
2016-12-13 14:37:46 -0700

    Footer r29 updaye and goldbar adjust test

--- footer.php
+++ footer.php
@@ -56,7 +56,7 @@
                 </nav>
                 <div class="copyright">
                     <p><?php echo get_field('footer_copyright_text','option'); ?></p>
-                    <p>Site design by <a href="#">Rule29</a>.</p>
+                    <p>Site design by <a href="http://rule29.com">Rule29</a>.</p>
                 </div>
             </div>
 
