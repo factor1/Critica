57c0c11c29667659cf6169d47e0dff7cc1e0a80b
Matt Adams <mattadams@factor1studios.com>
2017-01-30 14:27:39 -0700

    Author bio patched to fix james code

--- single.php
+++ single.php
@@ -10,17 +10,18 @@ get_header();
 ?>
 
 <section class="page single interior-d">
-
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>   
     <?php get_template_part("parts/featured-image"); ?>
     <?php get_template_part("parts/post-header"); ?>
     <section class="flexible-content">
     <div class="row">
         <div class="small-centered small-24 medium-20 large-18 columns">
-	     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>   
+	     
 		 	<?php the_content();?>
-		 	<?php endwhile; endif; ?>
+		 	
         </div>
     </div>
+    <?php endwhile; endif; ?>
     </section>
     
     <?php get_template_part("parts/share-links"); ?>
