da41f491b01591d754a4a980f7327a1f7fb684cd
Alexander Persky <alexander@factor1studios.com>
2017-02-17 13:50:48 -0700

    Formatting

--- index.php
+++ index.php
@@ -7,12 +7,11 @@ get_header();
 
 ?>
 <section class="page archive">
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>  
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	    <?php get_template_part('parts/post-banner'); ?>
 	    <?php get_template_part('parts/post-filters'); ?>
 	    <?php get_template_part('parts/archive-grid'); ?>
-    <?php endwhile; endif; ?>
-
+  <?php endwhile; endif; ?>
 </section>
 <?php
 
